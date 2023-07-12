af874c16da88f4f4ce01c067f4c9ffb0c27de9ba
Vikas <vikasmourya54321@gmail.com>
2023-07-12 11:41:01 +0530

    Added a bit of Js and changed color theme

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multitext",{
+    strings: [" A Student"," A Web Developer"," A Beginner"," A Coder"," A Geek"],
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 900,
+    loop: true
+})
