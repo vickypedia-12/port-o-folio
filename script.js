9f6b6d8fe809f371aef3bb8b20ab4605b004044b
Ketan Kumavat <ketan.kumavat1984@gmail.com>
2023-10-12 22:11:49 +0530

    Single page
     Changes :
    	deleted:    About.html
    	modified:   index.html
    	new file:   linkedin-in.svg
    	deleted:    project.html
    	modified:   script.js
    	modified:   style.css

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-var typed = new Typed(".multitext",{
-    strings: [" A Student"," A Web Developer"," A Beginner"," A Coder"," A Geek"],
-    typeSpeed: 80,
-    backSpeed: 50,
-    backDelay: 900,
-    loop: true
-})
+var typed = new Typed(".multitext", {
+  strings: [
+    " A Student",
+    " A Web Developer",
+    " A Beginner",
+    " A Coder",
+    " A Geek",
+  ],
+  typeSpeed: 80,
+  backSpeed: 50,
+  backDelay: 900,
+  loop: true,
+});
+
+var navbar = document.getElementById("navbar");
+
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
